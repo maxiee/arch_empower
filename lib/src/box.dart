32c8140a94100819d4d64c47d473b5ca2dbe502b
Maxiee <maxieewong@gmail.com>
2024-06-01 10:48:44 +0800

    Box

---
+++ lib/src/box.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Box extends StatelessWidget {
+  const Box(
+      {super.key, required this.title, required this.color, this.height = 50});
+
+  final String title;
+  final Color color;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      margin: const EdgeInsets.all(4),
+      padding: const EdgeInsets.all(6),
+      decoration: BoxDecoration(
+        color: color,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Center(
+        child: Text(
+          title,
+        ),
+      ),
+    );
+  }
+}
