6517a1e0743b06c36f7f6d07247cf0ceb3f5a5b2
Maxiee <maxieewong@gmail.com>
2024-06-01 10:35:36 +0800

    Col and Row

---
+++ lib/src/row_col.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/widgets.dart';
+
+class R extends StatelessWidget {
+  const R({super.key, required this.children, this.flexes});
+
+  final List<Widget> children;
+  final List<int>? flexes;
+
+  @override
+  Widget build(BuildContext context) {
+    return IntrinsicHeight(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: children.map((e) {
+          if (flexes != null) {
+            return Expanded(flex: flexes![children.indexOf(e)], child: e);
+          }
+          return Expanded(child: e);
+        }).toList(),
+      ),
+    );
+  }
+}
+
+class C extends StatelessWidget {
+  const C({super.key, required this.children});
+
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisSize: MainAxisSize.min, children: children);
+  }
+}
